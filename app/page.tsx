1fae70ce40a00fc78d56f73191d6315842b5c6b7
Zamil Bahri <zamilbahri@gmail.com>
2024-09-15 22:48:27 -0400

    implemented intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+import Intro from "@/components/intro";
+
 export default function Home() {
   return (
-		<main className="">
+		<main className="flex flex-col items-center px-4">
+			<Intro />
 		</main>
   );
 }
