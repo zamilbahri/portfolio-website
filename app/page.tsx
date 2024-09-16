8673f3ab3532be5a4c6e2afbecbc447a388b20f4
Zamil Bahri <zamilbahri@gmail.com>
2024-09-16 15:04:13 -0400

    added section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
 		<main className="flex flex-col items-center px-4">
 			<Intro />
+			<SectionDivider />
 		</main>
   );
 }
