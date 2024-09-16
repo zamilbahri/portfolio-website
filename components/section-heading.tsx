9a72c19d337beabba3ac0df474fcf0cb755ae234
Zamil Bahri <zamilbahri@gmail.com>
2024-09-16 16:30:51 -0400

    added prop type

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const SectionHeading = ({ children }) => {
+type SectionHeadingProps = {
+	children: React.ReactNode;
+}
+
+const SectionHeading = ({ children }: SectionHeadingProps) => {
 	return (
 		<h2 className='text-3xl font-medium capitalize mb-8' >{children}</h2>
 	)
