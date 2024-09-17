5eb38e8bd7b16a74bcc820fdcb5161c7770e1132
zamilbahri <zamilbahri@gmail.com>
2024-09-17 17:54:53 -0400

    added id='about' and added padding when jumping to this id

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,7 +5,7 @@ import Project from './project'
 
 const Projects = () => {
 	return (
-		<section id="projects" className='scroll-mt-28 mb-28 text-center'>
+		<section id="projects" className='mb-28 text-center scroll-mt-28 '>
 			<SectionHeading>My Projects</SectionHeading>
 			<div>
 				{
