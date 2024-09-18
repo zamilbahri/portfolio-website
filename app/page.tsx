a3ab4f5c27f7d39e5b0a56053bcf2b350ea62349
zamilbahri <zamilbahri@gmail.com>
2024-09-18 19:42:22 -0400

    added skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import About from "@/components/about";
-import Intro from "@/components/intro";
-import Projects from "@/components/projects";
-import SectionDivider from "@/components/section-divider";
-import WorkInProgress from "@/components/wip";
+import About from '@/components/about';
+import Intro from '@/components/intro';
+import Projects from '@/components/projects';
+import SectionDivider from '@/components/section-divider';
+import Skills from '@/components/skills';
+import WorkInProgress from '@/components/wip';
 
 export default function Home() {
   return (
-		<main className="flex flex-col items-center px-4">
-			<Intro />
-			<SectionDivider />
-			<About />
-			<Projects />
-			<WorkInProgress />
-		</main>
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <WorkInProgress />
+    </main>
   );
 }
