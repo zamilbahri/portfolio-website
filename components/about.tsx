86ef67e3cb5ebf63bb86679cf6734ed89456e634
Zamil Bahri <zamilbahri@gmail.com>
2024-09-19 01:52:29 -0400

    removed comment

--- components/about.tsx
+++ components/about.tsx
@@ -16,7 +16,6 @@ const About = () => {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      // ref={ref}
     >
       <SectionHeading>About Me</SectionHeading>
       <p>
