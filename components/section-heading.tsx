c75c6bbee7c4d9ffcb813827b0b0420ad528f0e8
Zamil Bahri <zamilbahri@gmail.com>
2024-09-18 23:01:07 -0400

    formatting

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 type SectionHeadingProps = {
-	children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
 const SectionHeading = ({ children }: SectionHeadingProps) => {
-	return (
-		<h2 className='text-3xl font-medium capitalize mb-8' >{children}</h2>
-	)
-}
+  return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>;
+};
 
-export default SectionHeading
+export default SectionHeading;
