5f254eb545c8a3cde6be5a22c3c0d96c1d58ead1
Zamil Bahri <zamilbahri@gmail.com>
2024-09-22 01:13:12 -0400

    refactored verticaltimeline and it somehow fixed icon background issue and scrolling animation

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,58 +1,75 @@
 'use client';
-
+import React from 'react';
 import { useSectionInView } from '@/lib/hooks';
-import SectionHeading from './section-heading';
-import 'react-vertical-timeline-component/style.min.css';
-import { experiencesData } from '@/lib/data';
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import { experiencesData } from '@/lib/data';
+import { useInView } from 'react-intersection-observer';
 import { useTheme } from '@/context/theme-context';
-import React from 'react';
+import SectionHeading from './section-heading';
 
-const Experience = () => {
-  const { ref, inView } = useSectionInView('Experience', 0.25);
-  const { theme } = useTheme();
+type ExperienceElementProps = {
+  theme: string;
+  item: {
+    date: string;
+    icon: React.ReactNode;
+    title: string;
+    location: string;
+    description: string;
+  };
+};
 
+const ExperienceElement = ({ theme, item }: ExperienceElementProps) => {
+  const { ref, inView } = useInView({ threshold: 0 });
   return (
-    <section
-      id="experience"
-      ref={ref}
-      className="scroll-mt-28 mb-28 sm:mb-40 text-center"
-    >
+    <div ref={ref} className="vertical-timeline-element">
+      <VerticalTimelineElement
+        visible={inView}
+        contentStyle={{
+          background:
+            theme === 'light' ? '#f3f4f6' : 'rgba(255, 255, 255, 0.05)',
+          boxShadow: 'none',
+          border: '1px solid rgba(0, 0, 0, 0.05)',
+          textAlign: 'left',
+          padding: '1.3rem 2rem',
+        }}
+        contentArrowStyle={{
+          borderRight:
+            theme === 'light'
+              ? '0.4rem solid #9ca3af'
+              : '0.4rem solid rgba(255, 255, 255, 0.5)',
+        }}
+        date={item.date}
+        icon={item.icon}
+        iconStyle={{
+          background: theme === 'light' ? 'white' : 'rgba(29, 36, 50, 0.95)',
+          fontSize: '1.5rem',
+        }}
+      >
+        <h3 className="font-semibold capitalize">{item.title}</h3>
+        <p className="font-normal !mt-0">{item.location}</p>
+        <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+          {item.description}
+        </p>
+      </VerticalTimelineElement>
+    </div>
+  );
+};
+
+export default function Experience() {
+  const { ref } = useSectionInView('Experience');
+  const { theme } = useTheme();
+  return (
+    <section ref={ref} id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My Experience</SectionHeading>
-      <VerticalTimeline lineColor="#e5e7eb">
+      <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
-          <React.Fragment key={index}>
-            <VerticalTimelineElement
-              visible={inView}
-              contentStyle={{
-                background:
-                  theme === 'light' ? '#f3f4f6' : 'rgba(255, 255, 255, 0.05)',
-                boxShadow: 'none',
-                border: '1px solid rgb(0, 0, 0, 0.05)',
-                textAlign: 'left',
-                padding: '1.3rem 2rem',
-              }}
-              contentArrowStyle={{
-                borderRight: '0.4rem solid #9ca3af',
-              }}
-              date={item.date}
-              icon={item.icon}
-            >
-              <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700">
-                {item.description}
-              </p>
-            </VerticalTimelineElement>
-          </React.Fragment>
+          <ExperienceElement key={index} theme={theme} item={item} />
         ))}
       </VerticalTimeline>
     </section>
   );
-};
-
-export default Experience;
+}
